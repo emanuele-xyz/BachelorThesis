8a7befb910495b4eb902b768608496655a2f3c4c
emanuele <me.emanuele.xyz@gmail.com>
2022-02-16 17:18:16 +0100

    Review depth testing chapter

--- src/ChDepthTesting/DepthTestRenderPassSubpass.cpp
+++ src/ChDepthTesting/DepthTestRenderPassSubpass.cpp
@@ -1,10 +1,6 @@
-// color attachment reference ...
-
 VkAttachmentReference depthAttachmentReference = {};
 depthAttachmentReference.attachment = 1;
 depthAttachmentReference.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-
-// color attachment references ...
 
 VkSubpassDescription colorSubpass = {};
 colorSubpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
