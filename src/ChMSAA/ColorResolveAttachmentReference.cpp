4c204e19830f0f436250e8d987fb42da979c6f9e
emanuele <me.emanuele.xyz@gmail.com>
2022-02-23 16:30:25 +0100

    Review MSAA chapter

--- src/ChMSAA/ColorResolveAttachmentReference.cpp
+++ src/ChMSAA/ColorResolveAttachmentReference.cpp
@@ -1,3 +1,7 @@
 VkAttachmentReference colorAttachmentResolveReference = {};
 colorAttachmentResolveReference.attachment = 2;
 colorAttachmentResolveReference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+// ...
+
+colorSubpass.pResolveAttachments = &colorAttachmentResolveReference;
