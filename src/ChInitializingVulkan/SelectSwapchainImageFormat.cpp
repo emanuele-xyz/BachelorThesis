60402eaba065bbeb83212ca508000df8ec642a96
emanuele <me.emanuele.xyz@gmail.com>
2022-03-07 12:55:50 +0100

    Review Introduction, Chapter 01 and Chapter 02

--- src/ChInitializingVulkan/SelectSwapchainImageFormat.cpp
+++ src/ChInitializingVulkan/SelectSwapchainImageFormat.cpp
@@ -6,7 +6,6 @@ if ((formatCount == 1) && (formats[0].format == VK_FORMAT_UNDEFINED))
 else
 {
     // We have to pick a format from the list
-    // We search for a format that we like
     for (u32 i = 0; i < formatCount; i++)
     {
         if (formats[i].format == VK_FORMAT_R8G8B8A8_UNORM)
